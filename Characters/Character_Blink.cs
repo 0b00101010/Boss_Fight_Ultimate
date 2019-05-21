fd860ee290cdefe12aab597b2b7baacb7bb82146
Twilight <twilivez@gmail.com>
2019-05-21 23:45:49 +0900

    Develop new character blink's
    He uses energy to blink a short space.

---
+++ Characters/Character_Blink.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Blink : Character
+{
+    private float char_Speed;
+    private int char_Hp;
+    private int char_Energy;
+    private int char_AbilityPrice;
+    private int char_JumpForce;
+
+    private Blink ability = new Blink();
+
+
+    private void Start()
+    {
+        ability.Init();
+        char_Hp = 700;
+        char_Speed = 7.7f;
+        char_Energy = 100;
+        char_AbilityPrice = 33;
+        char_JumpForce = 5;
+
+        IDInit(5,5,5);
+        RankInit(0,2,4);
+        StatInit(char_Speed, char_Hp, char_Energy, char_AbilityPrice, char_JumpForce);
+    }
+
+    public override void SpecialAbility()
+    {
+        ability.Enter();
+        base.SpecialAbility();
+    }
+
+    public override void UnSpecialAbility()
+    {
+        base.UnSpecialAbility();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+}
---
+++ Characters/Skill/Blink.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour, ISkill
+{
+
+    private GameObject targetCharacter;
+    private int blinkDistance;
+    public void Init()
+    {
+        blinkDistance = 2;
+        targetCharacter = GameObject.FindWithTag("Character");
+    }
+
+    public void Enter()
+    {
+        if (targetCharacter.GetComponent<SpriteRenderer>().flipX)
+        {
+            if (targetCharacter.transform.position.x - blinkDistance > -8.3f)
+                targetCharacter.transform.Translate(new Vector2(-blinkDistance, 0));
+            else if (targetCharacter.transform.position.x - blinkDistance < -8.3f)
+                targetCharacter.transform.position = new Vector2 (-8.3f, targetCharacter.transform.position.y);
+
+        }
+        else if (targetCharacter.GetComponent<SpriteRenderer>().flipX == false)
+        {
+
+            if (targetCharacter.transform.position.x + blinkDistance < 8.3f)
+                targetCharacter.transform.Translate(new Vector2(blinkDistance, 0));
+            else if (targetCharacter.transform.position.x + blinkDistance > 8.3f)
+                targetCharacter.transform.position = new Vector2(8.3f, targetCharacter.transform.position.y);
+        }
+
+    }
+
+
+    public void Excute() { 
+    
+    }
+
+    public void Exit() { 
+    
+    }
+
+
+}
