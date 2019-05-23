ce28b2cad3eaae99cb60f90198ff1130a145be0a
Twilight <twilivez@gmail.com>
2019-05-23 20:59:49 +0900

    Develop new character jumper's
    He uses energy to jump high.

---
+++ Characters/Character_Jumper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Jumper : Character
+{
+    private float char_Speed;
+    private int char_Hp;
+    private int char_Energy;
+    private int char_AbilityPrice;
+    private int char_JumpForce;
+    private Jumper abilitySkill = new Jumper();
+    private void Start()
+    {
+        abilitySkill.Init();
+        char_Hp = 700;
+        char_Speed = 7.7f;
+        char_Energy = 100;
+        char_AbilityPrice = 40;
+        char_JumpForce = 5;
+
+        StatInit(char_Speed, char_Hp, char_Energy, char_AbilityPrice, char_JumpForce, abilitySkill);
+        RankInit(0, 1, 5);
+        //ID 값 변경
+        IDInit(6, 26, 6);
+
+    }
+}
---
+++ Characters/Skill/Jumper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour, ISkill
+{
+    private GameObject targetObject;
+
+    public void Init() {
+        targetObject = GameObject.FindWithTag("Character");
+    }
+
+    public bool Repeat() {
+        return false;
+    }
+
+
+    public void Enter() {
+        Vector2 force = new Vector2(0, 14);
+        Rigidbody2D rBody = targetObject.GetComponent<Rigidbody2D>();
+        rBody.velocity = force;
+    }
+
+    public void Excute() {
+
+    }
+
+    public void Exit() { 
+    
+    }
+
+}
