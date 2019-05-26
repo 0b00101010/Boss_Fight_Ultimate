f696cfb1503243699891ae57a391eda563453df5
Twilight <twilivez@gmail.com>
2019-05-26 18:43:38 +0900

    Creating character portal.

---
+++ Characters/Character_Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Portal : Character
+{
+
+    private float char_Speed;
+    private int char_Hp;
+    private int char_Energy;
+    private int char_AbilityPrice;
+    private int char_JumpForce;
+
+    private void Start()
+    {
+        char_Hp = 800;
+        char_Speed = 7f;
+        char_Energy = 100;
+        char_AbilityPrice = 50;
+        char_JumpForce = 5;
+        //StatInit(char_Speed, char_Hp, char_Energy, char_AbilityPrice, char_JumpForce);
+        IDInit(7, 7, 7);
+        RankInit(0, 1, 5);
+    }
+}
