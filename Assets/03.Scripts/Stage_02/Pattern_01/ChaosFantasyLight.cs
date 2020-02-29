f8d6d46cb74a5ad019421dc96b2403a5a60676a6
전세훈 <twilivez@gmail.com>
2020-02-29 17:43:01 +0900

    기능 : ChaosFantasy 두번째 패턴 개발

---
+++ Assets/03.Scripts/Stage_02/Pattern_01/ChaosFantasyLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaosFantasyLight : Enemy
+{
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake(){
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public IEnumerator Execute(){
+        gameObject.SetActive(true);
+        yield return StartCoroutine(GameManager.instance.fadeManager.SpriteFadeOutCoroutine(spriteRenderer,0.25f));
+        Reset();
+    }
+    
+    private void Reset(){
+        spriteRenderer.color = Color.white;
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/03.Scripts/Stage_02/Pattern_01/LightPillar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPillar : BossPattern{
+
+    [SerializeField]
+    private int index; // 0 : column 1: rows
+    
+    private ChaosFantasyLight chaosFantasyLight;
+    
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake(){
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        chaosFantasyLight = gameObject.GetComponentInChildren<ChaosFantasyLight>(true);
+    }
+    
+    public override void Execute(){
+        if(index.Equals(0)){
+            NewPositionX();
+        }
+        else{
+            NewPositionY();
+        }
+        gameObject.SetActive(true);
+        StartCoroutine(ExecuteCoroutine());
+    }
+
+    private IEnumerator ExecuteCoroutine(){
+        yield return StartCoroutine(GameManager.instance.fadeManager.SpriteFadeOutCoroutine(spriteRenderer,0.1f));
+        yield return StartCoroutine(chaosFantasyLight.Execute());
+        Reset();
+    }
+
+    private void Reset(){
+        spriteRenderer.color = Color.white;
+        gameObject.SetActive(false);
+    }
+}
